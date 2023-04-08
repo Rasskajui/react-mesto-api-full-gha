3bb8ad8b987a920f059c38704b22bfc174724465
rasskajui <nana7305@mail.ru>
2023-04-08 21:24:12 +0300

    cors fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,14 +12,14 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();

6d462d811c5ecf00a42e0c356bef2f14ab18e55b
rasskajui <nana7305@mail.ru>
2023-04-09 13:27:31 +0300

    cards fix

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,8 +4,8 @@ import CurrentUserContext from "../contexts/CurrentUserContext";
 function Card(props) {
     const currentUser = useContext(CurrentUserContext);
     
-    const isOwn = props.card.owner._id === currentUser._id;
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isOwn = props.card.owner === currentUser._id;
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
     const cardLikeButtonClassName = ( 
         `gallery__image-like-btn ${isLiked && 'gallery__image-like-btn_active'}` 
     );
